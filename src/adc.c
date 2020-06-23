e4e516a25d85c5e4655620331f7f9fcd86f00f70
Andrew Jeddeloh <andrew.jeddeloh@gmail.com>
2020-06-22 20:02:40 -0700

    adc: minor cleanup, switch up the pins
    Use PA0, PA1 for ADC1 with plans for PA2, PA3 for ADC2 in the future.

--- src/adc.c
+++ src/adc.c
@@ -2,32 +2,35 @@
 
 #include "adc.h"
 
-#define CHAN1 6
+#define CHAN1 5 // PA0 = V-, PA1 = V+
+#define CHAN2 7 // PA2 = V-, PA3 = V+
 
 static uint16_t data[ADC_BUF_LEN];
 
 void init_adc() {
-
+	// Enable Interrupts
 	NVIC_EnableIRQ(ADC1_2_IRQn);
 	NVIC_EnableIRQ(DMA1_Channel1_IRQn);
-	// Init the clock
+
+	// Init the clocks for ADC, GPIOA, DMA1
 	RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN | RCC_AHB2ENR_GPIOAEN;
 	RCC->AHB1ENR |= RCC_AHB1ENR_DMA1EN;
 	ADC123_COMMON->CCR |= ADC_CCR_CKMODE;
 	
-	// Init the GPIO (PA1, PA2)
+	// Init the GPIO (PA0-PA3)
 	// Would be good to use any of the PC0-3 pins since they're on fast
 	// ADC channels, but the Disco board uses them for other things instead.
 	// Should be in analog mode after reset already
-	GPIOA->ASCR = GPIO_ASCR_ASC1 | GPIO_ASCR_ASC2;
+	GPIOA->ASCR = GPIO_ASCR_ASC0 | GPIO_ASCR_ASC1
+		| GPIO_ASCR_ASC2 | GPIO_ASCR_ASC3;
 
 	// Init the DMA
 	// ADC1, Chan 1 defaults to ADC1
 	DMA1_Channel1->CNDTR = ADC_BUF_LEN;
 	DMA1_Channel1->CPAR = (uint32_t)&ADC1->DR;
 	DMA1_Channel1->CMAR = (uint32_t)data;
-	DMA1_Channel1->CCR |= (3 << DMA_CCR_PL_Pos) | (1 << DMA_CCR_MSIZE_Pos)
-		| (1 << DMA_CCR_PSIZE_Pos) | DMA_CCR_MINC | DMA_CCR_EN | DMA_CCR_TEIE;
+	DMA1_Channel1->CCR |= (3 << DMA_CCR_PL_Pos) | (DMA_CCR_MSIZE_0)
+		| (DMA_CCR_PSIZE_0) | DMA_CCR_MINC | DMA_CCR_EN | DMA_CCR_TEIE;
 
 	// Init the adc
 	// turn off deep power down DEEPPWD=0 and turn on the voltage
@@ -37,7 +40,7 @@ void init_adc() {
 	HAL_Delay(ADC_SETUP_TIME_MS);
 	ADC1->SMPR1 = 1; // 6.5cycles for channel 1
 	ADC1->SQR1 = (CHAN1 << ADC_SQR1_SQ1_Pos);
-	ADC1->DIFSEL = ADC_DIFSEL_DIFSEL_6;
+	ADC1->DIFSEL = 1 << CHAN1;
 	ADC1->OFR1 = ADC_OFR1_OFFSET1_EN | (CHAN1 << ADC_OFR1_OFFSET1_CH_Pos) | 2048;
 
 	// Calibrate
